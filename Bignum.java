3224632bf5a820b6d6b1ec4243e8936da0648f68
gayathriksrctece <gayathri301485@gmail.com>
2017-06-09 18:22:14 +0530

    Create Bignum.java

---
+++ Bignum.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+public class Bignum
+{
+public static void main(String args[])
+{
+int a=578;
+int b=199;
+int c=345;
+if(a>b)
+{
+    if(a>c)
+    {
+        System.out.println(a);
+    }
+    else
+    {
+        System.out.println(c);
+    }
+}
+else
+{
+ if(b>c)
+ {
+     System.out.println(b);
+ }
+ System.out.println(c);
+}
+}    
+}
